925d2012301103666e6a7e46604bf669973bd381
Kevinnn28 <157818583+kevin2807-cmd@users.noreply.github.com>
2024-10-02 23:01:39 +0700

    Create bid.java

---
+++ Auction/bid.java
@@ -0,0 +1,18 @@
+
+public class Bid {
+    private Person bidder;
+    private double value;
+
+    public Bid(Person bidder, double value) {
+        this.bidder = bidder;
+        this.value = value;
+    }
+
+    public Person getBidder() {
+        return bidder;
+    }
+
+    public double getValue() {
+        return value;
+    }
+}
