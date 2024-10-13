c022ca27c2619bacdd4f465877f1f03bf54f7f6f
Kevinnn28 <157818583+kevin2807-cmd@users.noreply.github.com>
2024-10-13 18:25:27 +0700

    Create Storable.java

---
+++ TugasKelompok/TokoHandphone/Storable.java
@@ -0,0 +1,7 @@
+public interface Storable
+{
+    public void tambahItem (String nama_item, int jumlah);
+    public void hapusItem (String nama_item);
+    public void tampilkanItem ();
+    public boolean cekItem(String nama_item);
+}
